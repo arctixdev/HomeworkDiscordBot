b34fda66e278dc1c632332dda06993eed9224fa8
Jonathan Bangert <jonathan@bangert.dk>
2022-09-22 12:58:13 +0200

    Create project

---
+++ src/bot.ts
@@ -0,0 +1,12 @@
+import { Client } from "discord.js";
+
+console.log("Bot is starting...");
+
+const TOKEN = "MTAyMjQ1ODQyMTQxODY1OTg5MQ.GLyzun.7F5LxcvYt-WFONFMzI9YxOzZRpYWDarIvrQ6NM";
+
+const client = new Client({
+    intents: []
+});
+
+console.log(client); 
+
