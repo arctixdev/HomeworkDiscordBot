2f1ad46fc388840e00ae1a6844ac2a3e790c2b84
Jonathan Bangert <jonathan@bangert.dk>
2022-10-27 18:09:39 +0200

    OMG IT ACTUALLY WORKS IT WASNT A COMPLETE WASTE OF FREAKING 2 HOURS YAAAAAAAAAA

--- mariadb-test.py
+++ mariadb-test.py
@@ -1,23 +1,44 @@
 import mariadb, os
 from dotenv import load_dotenv
+from sys import exit
+from os import getenv
 
 load_dotenv()
-db_user = os.getenv("DBUSER")
-db_password = os.getenv("DBPASSWORD")
-db_host = os.getenv("DBHOST")
-db_name = os.getenv("DBNAME")
+db_user = getenv("DBUSER")
+db_password = getenv("DBPASSWORD")
+db_host = getenv("DBHOST")
+db_name = getenv("DBNAME")
 
-print(db_host, db_user, db_name, db_password)
-
-conn_params= {
-    "user" : db_user,
-    "password" : db_password,
-    "host" : db_host,
-    "database" : db_name
+conn_params = {
+    "user": db_user,
+    "password": db_password,
+    "host": db_host,
+    "database": db_name,
 }
+try:
+    database = mariadb.connect(**conn_params)
+    cursor = database.cursor()
+except SyntaxError as e:
+    print("ERROR COULD NOT CONNECT TO DATABASE... ERROR: {0}".format(e))
+    exit()
 
-database = mariadb.connect(**conn_params)
-cursor = database.cursor()
+
+def dbrun(cmd, arg):
+    print(f"Executing: {cmd}, {arg}")
+    cursor.execute(cmd, arg)
+    row = cursor.fetchall()
+    return row
+
+
+tag = input("Discord tag: ")
+id = dbrun("SELECT id FROM people WHERE tag=?", (tag,))[0][0]
+print(id)
+rem = dbrun("SELECT homeworkid FROM todo WHERE personid=?", (id,))
+print(rem)
+for i in rem:
+    print(i[0])
+    hm = dbrun("SELECT name FROM homework WHERE id=?", (i[0],))
+    print(hm)
 
 cursor.close()
 database.close()
